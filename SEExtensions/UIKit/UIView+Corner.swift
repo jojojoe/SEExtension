642c4b5a5ff798acd31b5f010d566e59398e04a3
轩张 <zx804463232@gmail.com>
2020-03-25 10:56:13 +0800

    添加View CornerMask

---
+++ SEExtensions/UIKit/UIView+Corner.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+Corner.swift
+//  SEExtensionDemo
+//
+//  Created by JOJO on 2020/3/25.
+//  Copyright © 2020 JOJO. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIView {
+    func addCornerMask(corners: UIRectCorner, cornerRadii: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds,
+                                byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: 22, height: 22))
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = bounds
+        maskLayer.path = path.cgPath
+        layer.mask = maskLayer
+    }
+}
+
